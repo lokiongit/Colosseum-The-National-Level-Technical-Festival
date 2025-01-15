7785bd7b4c27b9332a5ab3ecd0e2c2bf8d86223a
Lokesh Bhatt <lokeshbhatt187@gmail.com>
2025-01-15 17:19:06 +0530

    Update AboutUs.jsx

--- Frontend/src/Components/AboutUs.jsx
+++ Frontend/src/Components/AboutUs.jsx
@@ -4,7 +4,7 @@ import Image from "../assets/colo1.webp"
 const AboutUs = () => {
   return (
     <div className='bg-black p-4 ' id="colo">
-      <h1 className='my-10 font-bold p-5 text-white font-serif text-4xl text-center'>ABOUT COLOSSEUM</h1>
+      <h1 className='my-10 font-bold p-5 text-white font-serif text-4xl text-center'>ABOUT US</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-opacity-0 p-5">
         <div className="flex items-center mx-10">
           <p className="text-white text-lg">The only Society taking responsibility to organize all the major and minor events in College of Technology Pantnagar. The ETS has organized various national level events which enhance the personality of the students along with academics. It provides an exposure to students by creating ferocious competitive environment thereby acting as a motivation to excel in their areas of interest.</p>
